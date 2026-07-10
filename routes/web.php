69d3f33808d95dfb0bf9368ddc0a919cc8df5e7f
ramaditya234 <ramadityajoyaperdana74857@gmail.com>
2026-07-10 10:20:46 +0700

    auth login & register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+// Auth Routes (Hanya untuk yang belum login)
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+// Logout (Harus login terlebih dahulu)
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+
+// Area khusus Admin
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return "Ini adalah Halaman Admin. Layout sidebar gelap akan diterapkan di sini.";
+    })->name('admin.dashboard');
 });
 
 // Area khusus Admin
